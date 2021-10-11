3a81e6a1266414a56043ee5916212aa269efa81e
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-10-11 11:25:33 +0100

    Display Spinner while fetching products from API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import GlobalStyle from './globalStyles';
 import Navbar from './components/Navbar/index';
+import Spinner from './components/Spinner';
 import Products from './views/Products/index';
 import Cart from './views/Cart/index';
 
 function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function addProductToCart(id) {
     const product = products.find((item) => item.id === id);
@@ -32,24 +34,24 @@ function App() {
     setCart(newCart);
   }
 
-  function fetchProducts() {
-    const data = [];
-
-    fetch("https://fakestoreapi.com/products/category/men's clothing")
-      .then((res) => res.json())
-      .then((json) => data.push(...json));
-
-    fetch("https://fakestoreapi.com/products/category/women's clothing")
-      .then((res) => res.json())
-      .then((json) => data.push(...json));
-
-    return data;
-  }
-
   // Fetch data from fakeAPI and store it when component mounts
   useEffect(() => {
-    const data = fetchProducts();
-    setProducts(data);
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      const menClothing = await fetch(
+        "https://fakestoreapi.com/products/category/men's clothing"
+      ).then((res) => res.json());
+
+      const womenClothing = await fetch(
+        "https://fakestoreapi.com/products/category/women's clothing"
+      ).then((res) => res.json());
+
+      setProducts([...menClothing, ...womenClothing]);
+      setIsLoading(false);
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -59,7 +61,11 @@ function App() {
         <Navbar logoText='Fake Store' itemNumber={cart.length} />
         <Switch>
           <Route exact path='/products'>
-            <Products list={products} onClick={addProductToCart} />
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <Products list={products} onClick={addProductToCart} />
+            )}
           </Route>
           <Route exact path='/cart'>
             <Cart
